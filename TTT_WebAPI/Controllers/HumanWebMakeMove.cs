a72c9dcf0e9256e8dd47bf1ee722a844ebf80f4d
Stuart Laughlin <Stuart.Laughlin@samaritanministries.org>
2014-06-10 14:38:44 -0500

    Showing GameServer stuff and Events.

---
+++ TTT_WebAPI/Controllers/HumanWebMakeMove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tic_Tac_Toe;
+
+namespace TTT_WebAPI.Controllers
+{
+    public class HumanWebMakeMove : IDetermineMove
+    {
+        public int GetPlayerMove(Board board, int turnNumber, IPlayer turnPlayer)
+        {
+            // Render a page to the browser / user
+            // 
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tic-Tac-Toe/TestGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tic_Tac_Toe
+{
+    public class TestAIPlayer
+    {
+        private readonly IDetermineMove _moveDeterminer;
+        private TestGame _game;
+
+        public TestAIPlayer(IDetermineMove moveDeterminer, TestGame game)
+        {
+            _moveDeterminer = moveDeterminer;
+            _game = game;
+
+            _game.SomethingHappened += _game_SomethingHappened;
+        }
+
+        void _game_SomethingHappened(object sender, SomethingHappenedArgs args)
+        {
+            var turn = _moveDeterminer.GetPlayerMove(args.Board, args.TurnNumber, args.Player);
+            _game.PlayTurn(turn);
+        }
+    }
+
+    public class TestGame
+    {
+        private readonly IList<string> _messages;
+        private readonly Guid _id;
+
+        public event SomethingHappened SomethingHappened;
+
+        protected virtual void OnSomethingHappened(SomethingHappenedArgs args)
+        {
+            var handler = SomethingHappened;
+            if (handler != null) handler(this, args);
+        }
+
+        public TestGame(string message)
+        {
+            _messages = new List<string> {message};
+            _id = Guid.NewGuid();
+        }
+
+        public IEnumerable<string> Messages
+        {
+            get { return _messages; }
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public void AddMessage(string message)
+        {
+            _messages.Add(message);
+            var args = new SomethingHappenedArgs();
+            OnSomethingHappened(args);
+        }
+
+        public void PlayTurn(object turn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public delegate void SomethingHappened(object sender, SomethingHappenedArgs args);
+
+    public class SomethingHappenedArgs : EventArgs
+    {
+        public IPlayer Player { get; set; }
+        public int TurnNumber { get; set; }
+        public Board Board { get; set; }
+    }
+}
